5f121ae07e8c223be4483eae75e13d0f3d3794af
Ron Bendicio <43603734+sencys24@users.noreply.github.com>
2018-12-10 15:53:36 +0100

    Add files via upload

---
+++ Website/contact-form.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = $_Post['name'];
+$visitor_email = $_Post['email'];
+$message = $_POST['message'];
+
+$email_from = 'michael.vanacker2@gmail.com';
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n".
+    "User Email: $visitor_email.\n".
+    "User Message: $message.\n";
+
+$to = "rabendicio@yahoo.com";
+
+$headers = "From: $email_form \r\n";
+
+$headears .= "Reply-To $visitor_email \r\n";
+
+mail($to,$email-subject,$email_body,$headers);
+
+header ("Location: hiswebsitefull.html");
+
+?>
